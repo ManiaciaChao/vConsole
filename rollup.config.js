3aed2b4369b69440c250a3e53efa28f176075a70
helioszhao <helioszhao@tencent.com>
2021-07-11 21:10:48 +0800

    refactor(build): fully remove webpack config

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,9 @@ import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 import { string } from "rollup-plugin-string";
 import styles from "rollup-plugin-styles";
+import license from "rollup-plugin-license";
+import pkg from "./package.json";
+import copy from "rollup-plugin-copy";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -28,19 +31,13 @@ export default defineConfig({
           transpileOnly: true,
         },
       }),
-      compilerOptions: {
-        // enable run-time checks when not in production
-        dev: !production,
-      },
+      compilerOptions: { dev: !production },
     }),
     resolve({
       browser: true,
       dedupe: ["svelte"],
     }),
-    // commonjs(),
-    commonjs({
-      include: "node_modules/**",
-    }),
+    commonjs(),
     typescript({
       sourceMap: !production,
       inlineSources: !production,
@@ -51,6 +48,26 @@ export default defineConfig({
       include: "src/**/*.html",
     }),
     production && terser(),
+    license({
+      sourcemap: !production,
+      banner: [
+        "vConsole v" + pkg.version + " (" + pkg.homepage + ")",
+        "",
+        "Tencent is pleased to support the open source community by making vConsole available.",
+        "Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.",
+        'Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at',
+        "http://opensource.org/licenses/MIT",
+        'Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.',
+      ].join("\n"),
+    }),
+    copy({
+      targets: [
+        {
+          src: Path.resolve(__dirname, "./src/vconsole.d.ts"),
+          dest: Path.resolve(__dirname, "./dist/vconsole.min.d.ts"),
+        },
+      ],
+    }),
   ],
   watch: {
     clearScreen: false,
